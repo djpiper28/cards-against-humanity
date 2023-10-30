634ca7609673deabf480d5688e030f3c378720b3
Danny Piper <djpiper28@gmail.com>
2023-10-30 20:26:45 +0000

    feat: cards can now be played

--- game_logic/player.go
+++ game_logic/player.go
@@ -1,20 +1,68 @@
 package game_logic
 
 import (
+	"errors"
 	"github.com/google/uuid"
+	"log"
+	"sync"
 )
 
 type Player struct {
 	Id          uuid.UUID
 	Name        string
-	Hand        []*WhiteCard
-	CurrentPlay *WhiteCard
+	Hand        map[uuid.UUID]*WhiteCard
+	CurrentPlay []*WhiteCard
 	Connected   bool
+	lock        sync.Mutex
 }
 
 func NewPlayer(Name string) *Player {
 	return &Player{Id: uuid.New(),
 		Name:      Name,
-		Hand:      make([]*WhiteCard, 0),
+		Hand:      make(map[uuid.UUID]*WhiteCard),
 		Connected: true}
+}
+
+func (p *Player) hasCard(card *WhiteCard) bool {
+	_, found := p.Hand[card.Id]
+	return found
+}
+
+func (p *Player) PlayCard(cards []*WhiteCard) error {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	if p.CurrentPlay != nil {
+		return errors.New("Cannot play two cards")
+	}
+
+	for _, card := range cards {
+		if !p.hasCard(card) {
+			return errors.New("Cannot find the card in the hand")
+		}
+	}
+
+	cardsCopy := make([]*WhiteCard, len(cards))
+	copy(cardsCopy, cards)
+
+	p.CurrentPlay = cardsCopy
+
+	for _, card := range cards {
+		delete(p.Hand, card.Id)
+	}
+	return nil
+}
+
+func (p *Player) AddCardToHand(card *WhiteCard) error {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	if p.hasCard(card) {
+		msg := "Cannot add duplicate cards to the hand"
+		log.Println(msg)
+		return errors.New(msg)
+	}
+
+	p.Hand[card.Id] = card
+	return nil
 }
--- game_logic/player_test.go
+++ game_logic/player_test.go
@@ -46,3 +46,56 @@ func TestPlayerInit(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+func TestAddCard(t *testing.T) {
+	player := game_logic.NewPlayer("Dave")
+	cards := []*game_logic.WhiteCard{game_logic.NewWhiteCard(uuid.New(), "Testing 123"),
+		game_logic.NewWhiteCard(uuid.New(), "Testing 234"),
+		game_logic.NewWhiteCard(uuid.New(), "Testing 345"),
+		game_logic.NewWhiteCard(uuid.New(), "Testing 456"),
+		game_logic.NewWhiteCard(uuid.New(), "Testing 567"),
+	}
+
+	for _, card := range cards {
+		err := player.AddCardToHand(card)
+		if err != nil {
+			t.Log("Cannot add card to hand")
+			t.FailNow()
+		}
+	}
+
+	play := cards[0:2]
+	err := player.PlayCard(play)
+	if err != nil {
+		t.Log("Cannot play cards")
+		t.FailNow()
+	}
+
+	for _, card := range play {
+		_, found := player.Hand[card.Id]
+		if found {
+			t.Log("Card should have been removed as it was played")
+			t.FailNow()
+		}
+	}
+
+	for _, card := range cards[2:] {
+		_, found := player.Hand[card.Id]
+		if !found {
+			t.Log("Card should not have been removed as it wasn't played")
+			t.FailNow()
+		}
+	}
+
+	if len(player.CurrentPlay) != len(play) {
+		t.Log("Current play has wrong length")
+		t.FailNow()
+	}
+
+	for i, currentPlay := range player.CurrentPlay {
+		if currentPlay != cards[i] {
+			t.Log("Current play is not what is expected")
+			t.FailNow()
+		}
+	}
+}
