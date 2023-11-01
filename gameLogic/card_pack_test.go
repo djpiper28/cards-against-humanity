050ff1e811b57f9cc3bc609faba56558ea27d0ed
Danny Piper <danny.piper@bjss.com>
2023-11-01 13:38:22 +0000

    feat: added test for loading cards

--- gameLogic/card_pack_test.go
+++ gameLogic/card_pack_test.go
@@ -2,6 +2,7 @@ package gameLogic_test
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/djpiper28/cards-against-humanity/gameLogic"
@@ -24,6 +25,30 @@ func TestAccumalateCardPacks(t *testing.T) {
 
 	if len(deck.BlackCards) != expectedCards {
 		t.Log(fmt.Sprintf("Epected %d black cards, found %d", expectedCards, len(deck.BlackCards)))
+		t.FailNow()
+	}
+}
+
+func TestLoadCards(t *testing.T) {
+	os.Chdir("..")
+	err := gameLogic.LoadPacks()
+	if err != nil {
+		t.Log("Error whilst loading cards", err)
+		t.FailNow()
+	}
+
+	if len(gameLogic.AllBlackCards) == 0 {
+		t.Log("There are no black cards")
+		t.FailNow()
+	}
+
+	if len(gameLogic.AllWhiteCards) == 0 {
+		t.Log("There are no white ards")
+		t.FailNow()
+	}
+
+	if len(gameLogic.AllPacks) == 0 {
+		t.Log("There are no packs")
 		t.FailNow()
 	}
 }
