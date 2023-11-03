0af8b7e4cded10dbf0923a9f7012545ba3ea86c3
Danny Piper <danny.piper@bjss.com>
2023-11-03 12:43:48 +0000

    feat: added another test for getting games

--- main_test.go
+++ main_test.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 	"testing"
 	"time"
 
+	"github.com/djpiper28/cards-against-humanity/gameLogic"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -35,7 +37,7 @@ func TestGetMetrics(t *testing.T) {
 	assert.NotEmpty(t, body, "Body should not be empty")
 }
 
-func TestGetGamesNotFull(t *testing.T) {
+func TestGetGamesNotFullEmpty(t *testing.T) {
 	resp, err := http.Get(baseUrl + "/games/notFull")
 	assert.Nil(t, err, "There should not be an error getting the games")
 	assert.Equal(t, http.StatusOK, resp.StatusCode, "Should be OK")
@@ -43,4 +45,29 @@ func TestGetGamesNotFull(t *testing.T) {
 	body, err := io.ReadAll(resp.Body)
 	assert.Nil(t, err, "Should be able to read the body")
 	assert.Equal(t, string(body), "[]", "Should be an empty array")
+}
+
+func TestGetGamesNotFullOneGame(t *testing.T) {
+	name := "Dave"
+
+	gid, pid, err := GameRepo.CreateGame(gameLogic.DefaultGameSettings(), name)
+	assert.Nil(t, err, "Should be able to make a game")
+	assert.NotEmpty(t, gid)
+	assert.NotEmpty(t, pid)
+
+	resp, err := http.Get(baseUrl + "/games/notFull")
+	assert.Nil(t, err, "There should not be an error getting the games")
+	assert.Equal(t, http.StatusOK, resp.StatusCode, "Should be OK")
+
+	body, err := io.ReadAll(resp.Body)
+	assert.Nil(t, err, "Should be able to read the body")
+	assert.Equal(t, string(body), "[]", "Should be an empty array")
+
+	var games []gameLogic.GameInfo
+	err = json.Unmarshal(body, &games)
+	assert.Nil(t, err, "There should not be an error")
+	assert.Len(t, games, 1, "There should be one game")
+
+	assert.Equal(t, games[0].Id, gid, "Game Id should match")
+	assert.Len(t, games[0].PlayerCount, 1, "Should only be one player")
 }
