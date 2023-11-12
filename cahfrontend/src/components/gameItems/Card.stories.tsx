9340e72aced72cf7fb15c471669725244768a014
Danny Piper <djpiper28@gmail.com>
2023-11-12 23:25:26 +0000

    chore(refactor): stories are now just args defined as exported consts

--- cahfrontend/src/components/gameItems/Card.stories.tsx
+++ cahfrontend/src/components/gameItems/Card.stories.tsx
@@ -5,17 +5,19 @@ export default {
   component: Card,
 } as Meta;
 
-export const WhiteCard: Story = (args) => <Card {...args} />;
-WhiteCard.args = {
-  isWhite: true,
-  packName: "Cards Against Humanity",
-  cardText: "Menstrual rage.",
+export const WhiteCard = {
+  args: {
+    isWhite: true,
+    packName: "Cards Against Humanity",
+    cardText: "Menstrual rage.",
+  },
 };
 
-export const BlackCard: Story = (args) => <Card {...args} />;
-BlackCard.args = {
-  isWhite: false,
-  packName: "Cards Against Humanity",
-  cardText:
-    "Mr. and Mrs. Diaz, we called you in because we're concerned about Cynthia. Are you aware that your daughter is ___?",
+export const BlackCard = {
+  args: {
+    isWhite: false,
+    packName: "Cards Against Humanity",
+    cardText:
+      "Mr. and Mrs. Diaz, we called you in because we're concerned about Cynthia. Are you aware that your daughter is ___?",
+  },
 };
--- cahfrontend/src/components/inputs/Checkbox.stories.tsx
+++ cahfrontend/src/components/inputs/Checkbox.stories.tsx
@@ -5,16 +5,18 @@ export default {
   component: Checkbox,
 } as Meta;
 
-export const Checked: Story = (args) => <Checkbox {...args} />;
-Checked.args = {
-  label: "Example Ticked Checkbox",
-  checked: true,
-  onSetChecked: console.log,
+export const Checked = {
+  args: {
+    label: "Example Ticked Checkbox",
+    checked: true,
+    onSetChecked: console.log,
+  },
 };
 
-export const UnChecked: Story = (args) => <Checkbox {...args} />;
-Checked.args = {
-  label: "Example Unticked Checkbox",
-  checked: false,
-  onSetChecked: console.log,
+export const UnChecked = {
+  args: {
+    label: "Example Unticked Checkbox",
+    checked: false,
+    onSetChecked: console.log,
+  },
 };
