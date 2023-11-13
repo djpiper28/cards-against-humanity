f76d8b06e6d1e46f6192b80e19bacf6e5404354d
Danny Piper <djpiper28@gmail.com>
2023-11-13 20:41:29 +0000

    feat: added background to the card pack seleector

--- cahfrontend/src/pages/create.tsx
+++ cahfrontend/src/pages/create.tsx
@@ -28,45 +28,47 @@ export default function Create() {
         Create A Game of Cards Against Humanity
       </h1>
       <h2 class="text-xl">Choose Some Card Packs</h2>
-      <div class="flex flex-row flex-wrap gap-2 md:gap-1">
-        <For each={packs()}>
-          {(pack, index) => {
-            return (
-              <Checkbox
-                checked={pack.checked}
-                label={`${pack.name} (${
-                  pack.whiteCards + pack.blackCards
-                } Cards)`}
-                onSetChecked={(checked) => {
-                  if (checked && !selectedPacks().includes(pack.id)) {
-                    setSelectedPacks([...selectedPacks(), pack.id]);
-                  } else {
-                    setSelectedPacks(
-                      selectedPacks().filter((x) => x !== pack.id),
-                    );
-                  }
+      <div class="flex flex-col gap-5 rounded-2xl border-2 p-5 bg-gray-100">
+        <div class="flex flex-row flex-wrap gap-2 md:gap-1">
+          <For each={packs()}>
+            {(pack, index) => {
+              return (
+                <Checkbox
+                  checked={pack.checked}
+                  label={`${pack.name} (${
+                    pack.whiteCards + pack.blackCards
+                  } Cards)`}
+                  onSetChecked={(checked) => {
+                    if (checked && !selectedPacks().includes(pack.id)) {
+                      setSelectedPacks([...selectedPacks(), pack.id]);
+                    } else {
+                      setSelectedPacks(
+                        selectedPacks().filter((x) => x !== pack.id),
+                      );
+                    }
 
-                  const newPacks = structuredClone(packs());
-                  newPacks[index()].checked = !pack.checked;
-                  setPacks(newPacks);
-                }}
-              />
-            );
-          }}
-        </For>
+                    const newPacks = structuredClone(packs());
+                    newPacks[index()].checked = !pack.checked;
+                    setPacks(newPacks);
+                  }}
+                />
+              );
+            }}
+          </For>
+        </div>
+
+        <p class="text-xl">
+          {`You have ${selectedPacks()
+            .map((x) => packs().find((y) => y.id === x))
+            .filter((x) => !!x)
+            .map((x) => x.whiteCards)
+            .reduce((a, b) => a + b, 0)} white cards and ${selectedPacks()
+            .map((x) => packs().find((y) => y.id === x))
+            .filter((x) => !!x)
+            .map((x) => x.blackCards)
+            .reduce((a, b) => a + b, 0)} black cards.`}
+        </p>
       </div>
-
-      <p class="text-bold">
-        {`You have ${selectedPacks()
-          .map((x) => packs().find((y) => y.id === x))
-          .filter((x) => !!x)
-          .map((x) => x.whiteCards)
-          .reduce((a, b) => a + b, 0)} white cards and ${selectedPacks()
-          .map((x) => packs().find((y) => y.id === x))
-          .filter((x) => !!x)
-          .map((x) => x.blackCards)
-          .reduce((a, b) => a + b, 0)} black cards.`}
-      </p>
     </>
   );
 }
