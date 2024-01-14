c56af6612f4d2b63e54cb0f8a98445d563173648
Danny Piper <djpiper28@gmail.com>
2024-01-14 16:10:13 +0000

    feat: start dev server in e2e tests

--- e2e/chrome_driver.go
+++ e2e/chrome_driver.go
@@ -1,21 +1,8 @@
 package main
 
 import (
-	"log"
-	"os"
-	"path/filepath"
-
 	"github.com/go-rod/rod"
 )
-
-func GetBasePage() string {
-	wd, err := os.Getwd()
-	if err != nil {
-		log.Fatalf("Cannot get executable: %s", err)
-	}
-
-	return "file://" + filepath.Dir(wd) + "/cahfrontend/dist/"
-}
 
 func GetBrowser() *rod.Browser {
 	return rod.New().MustConnect()
--- e2e/service_starter.go
+++ e2e/service_starter.go
@@ -2,21 +2,56 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os/exec"
+	"time"
 )
 
 var started = false
-var process exec.Cmd = *exec.Command("../backend/backend")
+var backendProcess *exec.Cmd = exec.Command("../backend/backend")
+var frontendProcess *exec.Cmd = exec.Command("../cahfrontend/e2e-start.sh")
 
 func StartService() {
 	if started {
 		return
 	}
 
-	err := process.Start()
+  log.Println("Starting backend")
+	err := backendProcess.Start()
 	if err != nil {
 		log.Fatalf("Cannot start backend: %s", err)
 	}
 	log.Println("Started backend")
+
+  log.Println("Starting frontend")
+	err = frontendProcess.Start()
+	if err != nil {
+		log.Fatalf("Cannot start frontend: %s", err)
+	}
+	log.Println("Started frontend")
+
 	started = true
+}
+
+
+const frontendUrl = "http://localhost:3000/"
+func testFrontend() error {
+  _, err := http.Get(frontendUrl)
+  return err
+}
+
+func GetBasePage() string {
+  const maxTries = 10
+  for tries := 0; tries < maxTries; tries++ {
+    err := testFrontend()
+    if err != nil {
+      log.Printf("Try %d/%d failed: %s", tries + 1, maxTries, err)
+    } else {
+      break
+    }
+
+    time.Sleep(time.Second)
+  }
+
+  return frontendUrl
 }
