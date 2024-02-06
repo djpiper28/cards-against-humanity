c975efa58fe3dee2e4d19e628074f977fcd4e8fd
Danny Piper <djpiper28@gmail.com>
2024-02-06 23:24:25 +0000

    feat: added http upgrade test for ws

--- cahfrontend/src/gameState/websocketClient.test.ts
+++ cahfrontend/src/gameState/websocketClient.test.ts
@@ -1,22 +1,24 @@
 // @vitest-environment node
 import { v4 } from "uuid";
 import { toWebSocketClient } from "./websocketClient";
-import { beforeAll, describe, expect, it } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import WebSocket, { WebSocketServer } from "../../node_modules/ws/index.js";
+import { createServer } from "http";
+import { parse } from "url";
 
 function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-describe("WebSocketClient tests", () => {
+describe("WebSocketClient tests (ws dialup)", () => {
   const port = 1442;
   const onConnectMessage = `${v4()} test message from server`;
   const serverReceivedMessages: string[] = [];
+  const wss = new WebSocketServer({
+    port: port,
+  });
 
   beforeAll(() => {
-    const wss = new WebSocketServer({
-      port: port,
-    });
     wss.on("connection", (ws: WebSocket) => {
       ws.on("message", (msg: Buffer) => {
         console.error("A MESSAGE 1!!1", msg.toString());
@@ -28,12 +30,82 @@ describe("WebSocketClient tests", () => {
     });
   });
 
+  afterAll(() => {
+    wss.close();
+  });
+
   it("Test websocket", async () => {
     let onConnectCalled = false;
     let onDisconnectCalled = false;
     const clientReceivedMessages: string[] = [];
 
     const ws = new WebSocket(`ws://localhost:${port}`);
+    const wsClient = toWebSocketClient(ws, {
+      onConnect: () => {
+        onConnectCalled = true;
+        wsClient.sendMessage(msg);
+      },
+      onDisconnect: () => {
+        onDisconnectCalled = true;
+      },
+      onReceive: (msg: string) => {
+        clientReceivedMessages.push(msg);
+      },
+    });
+
+    const msg = `${v4()} test message from client`;
+
+    await delay(100);
+    expect(onConnectCalled).toBe(true);
+    expect(clientReceivedMessages).toContain(onConnectMessage);
+    expect(serverReceivedMessages).toContain(msg);
+
+    ws.close();
+    await delay(100);
+    expect(onDisconnectCalled).toBe(true);
+  });
+});
+
+describe("WebSocketClient tests (http upgrade)", () => {
+  const port = 1443;
+  const onConnectMessage = `${v4()} test message from server`;
+  const serverReceivedMessages: string[] = [];
+  const wss = new WebSocketServer({
+    noServer: true,
+  });
+  const server = createServer();
+
+  beforeAll(() => {
+    wss.on("connection", (ws: WebSocket) => {
+      ws.on("message", (msg: Buffer) => {
+        console.error("A MESSAGE 1!!1", msg.toString());
+        serverReceivedMessages.push(msg.toString());
+        console.error(serverReceivedMessages);
+      });
+      ws.on("error", console.error);
+      ws.send(onConnectMessage);
+    });
+    server.on("upgrade", function upgrade(request, socket, head) {
+      if (parse(request.url).pathname === "/foo") {
+        wss.handleUpgrade(request, socket, head, function done(ws) {
+          wss.emit("connection", ws, request);
+        });
+      }
+    });
+    server.listen(port);
+  });
+
+  afterAll(() => {
+    wss.close();
+    server.close();
+  });
+
+  it("Test websocket", async () => {
+    let onConnectCalled = false;
+    let onDisconnectCalled = false;
+    const clientReceivedMessages: string[] = [];
+
+    const ws = new WebSocket(`http://localhost:${port}/foo`);
     const wsClient = toWebSocketClient(ws, {
       onConnect: () => {
         onConnectCalled = true;
