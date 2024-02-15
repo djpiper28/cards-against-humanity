15f3397522669e034ffb41c7c4ed9aa977f5a7f6
Danny Piper <djpiper28@gmail.com>
2024-02-15 18:02:21 +0000

    feat: added tests for Card

---
+++ cahfrontend/src/components/gameItems/Card.test.tsx
@@ -0,0 +1,18 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@solidjs/testing-library";
+import Card, { Props } from "./Card";
+
+describe("Card", () => {
+  it("Should render the card data", () => {
+    const card: Props = {
+      isWhite: true,
+      cardText:
+        "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+      packName: "Poo poo",
+    };
+    render(<Card {...card} />);
+    expect(screen.getByText(card.cardText)).toBeDefined();
+    expect(screen.getByText("@" + card.packName)).toBeDefined();
+    expect(screen.getByTestId("white")).toBeDefined();
+  });
+});
--- cahfrontend/src/components/gameItems/Card.tsx
+++ cahfrontend/src/components/gameItems/Card.tsx
@@ -1,4 +1,4 @@
-interface Props {
+export interface Props {
   isWhite: boolean;
   cardText: string;
   packName: string;
@@ -11,6 +11,8 @@ export default function Card({ isWhite, cardText, packName }: Readonly<Props>) {
   return (
     <div
       class={`${textColour} ${backgroundColour} rounded-2xl p-3 md:p6 flex flex-col justify-between h-80 w-64 border-2 text-left`}
+      aria-label={`${isWhite ? "white" : "black"} card`}
+      data-testid={`${isWhite ? "white" : "black"}`}
     >
       <p class="font-bold text-lg">{cardText}</p>
       <div class="text-xs">@{packName}</div>
