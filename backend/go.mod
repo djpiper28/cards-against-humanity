1d2f75b75a2c711d3537e4db4284061babd595e2
Danny Piper <djpiper28@gmail.com>
2024-02-29 12:44:13 +0000

    feat: added CORs middleware

--- backend/go.mod
+++ backend/go.mod
@@ -22,6 +22,7 @@ require (
 	github.com/chenzhuoyu/iasm v0.9.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/gin-contrib/cors v1.5.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-openapi/jsonpointer v0.20.2 // indirect
 	github.com/go-openapi/jsonreference v0.20.4 // indirect
--- backend/main.go
+++ backend/main.go
@@ -6,6 +6,7 @@ import (
 
 	gpmiddleware "github.com/carousell/gin-prometheus-middleware"
 	docs "github.com/djpiper28/cards-against-humanity/backend/docs"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -15,33 +16,40 @@ import (
 func Start() {
 	log.SetFlags(log.Lshortfile | log.Ltime | log.Ldate)
 	log.Println("Starting up Cards Against Humanity server")
-	r := gin.Default()
+	router := gin.Default()
 
 	// Setup swagger
 	docs.SwaggerInfo.BasePath = "/"
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	// System routes
-	r.GET("/healthcheck", func(c *gin.Context) {
+	router.GET("/healthcheck", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"healthy": true,
 		})
 	})
 
 	// Add metrics for the amount of games and users
-	r.GET("/game-metrics", func(c *gin.Context) {
+	router.GET("/game-metrics", func(c *gin.Context) {
 		c.TOML(http.StatusOK, GetMetrics())
 	})
 
 	// Add metrics for the performance of Gin
-	p := gpmiddleware.NewPrometheus("cards_against_humanity")
-	p.Use(r)
+	prometheusEndpoint := gpmiddleware.NewPrometheus("cards_against_humanity")
+	prometheusEndpoint.Use(router)
+
+	// Add CORS middleware, this also allows for other clients to use this server
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{"*"},
+		AllowMethods: []string{"PUT", "PATCH", "DELETE", "GET"},
+	}))
 
 	// Setup all endpoints
-	SetupGamesEndpoints(r)
-	SetupResoucesEndpoints(r)
+	SetupGamesEndpoints(router)
+	SetupResoucesEndpoints(router)
 
-	r.Run()
+	router.Run()
 }
 
 // @title			Cards Against Humanity API
