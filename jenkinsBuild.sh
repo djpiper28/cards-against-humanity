61d948d9726680a8724e16817ef1d003e0ca6999
Danny Piper <djpiper28@gmail.com>
2024-03-10 14:47:54 +0000

    fix: url

--- jenkinsBuild.sh
+++ jenkinsBuild.sh
@@ -3,7 +3,7 @@ export GOPATH=/var/lib/jenkins/go
 export PATH=$PATH:/usr/local/bin
 export PATH=$PATH:$GOPATH/bin
 
-export VITE_API_BASE_URL=https://cards.djpiper28.co.uk/api/
+export VITE_API_BASE_URL=https://cards.djpiper28.co.uk/api
 export VITE_WS_BASE_URL=wss://cards.djpiper28.co.uk/api/games/join
 
 make frontend -j
