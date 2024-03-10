f599bb1e2806b426c4615066ce5e375869070a60
Danny Piper <djpiper28@gmail.com>
2024-03-10 14:44:57 +0000

    fix: path should be set correctly

--- jenkinsBuild.sh
+++ jenkinsBuild.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-export PATH=/var/lib/jenkins/go/bin:$PATH
 export GOPATH=/var/lib/jenkins/go
+export PATH=$PATH:$GOPATH/bin
 
 export VITE_API_BASE_URL=https://cards.djpiper28.co.uk/
 export VITE_WS_BASE_URL=wss://cards.djpiper28.co.uk/api/games/join
