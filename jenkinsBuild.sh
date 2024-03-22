9dc7b8ff1a176a7e61bf16c51c167421c7a98ee5
Danny Piper <djpiper28@gmail.com>
2024-03-22 10:44:37 +0000

    fix: export env vars because that way it means that make has them lol

--- jenkinsBuild.sh
+++ jenkinsBuild.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-GOPATH=/var/lib/jenkins/go
-PATH=$PATH:/usr/local/bin
-PATH=$PATH:$GOPATH/bin
+export GOPATH=/var/lib/jenkins/go
+export PATH=$PATH:/usr/local/bin
+export PATH=$PATH:$GOPATH/bin
 
-BASE_URL=cards.djpiper28.co.uk/api
-VITE_API_BASE_URL=https://$BASE_URL
-VITE_WS_BASE_URL=wss://$BASE_URL/join
+export BASE_URL=cards.djpiper28.co.uk/api
+export VITE_API_BASE_URL=https://$BASE_URL
+export VITE_WS_BASE_URL=wss://$BASE_URL/join
 
 make -j
