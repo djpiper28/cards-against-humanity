a3c63153e5ae080c0ef8b42fa7d197d79d641899
Danny Piper <djpiper28@gmail.com>
2024-03-22 10:50:32 +0000

    fix: /games/

--- jenkinsBuild.sh
+++ jenkinsBuild.sh
@@ -5,6 +5,6 @@ export PATH=$PATH:$GOPATH/bin
 
 export BASE_URL=cards.djpiper28.co.uk/api
 export VITE_API_BASE_URL=https://$BASE_URL
-export VITE_WS_BASE_URL=wss://$BASE_URL/join
+export VITE_WS_BASE_URL=wss://$BASE_URL/games/join
 
 make -j
