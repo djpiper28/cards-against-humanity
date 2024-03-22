bc434495208083649ca3d98ba31732e9634251cd
Danny Piper <djpiper28@gmail.com>
2024-03-22 09:14:07 +0000

    fix: correct url for ws base url

--- jenkinsBuild.sh
+++ jenkinsBuild.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-export GOPATH=/var/lib/jenkins/go
-export PATH=$PATH:/usr/local/bin
-export PATH=$PATH:$GOPATH/bin
+GOPATH=/var/lib/jenkins/go
+PATH=$PATH:/usr/local/bin
+PATH=$PATH:$GOPATH/bin
 
-export VITE_API_BASE_URL=https://cards.djpiper28.co.uk/api
-export VITE_WS_BASE_URL=wss://cards.djpiper28.co.uk/api/games/join
+BASE_URL=cards.djpiper28.co.uk/api
+VITE_API_BASE_URL=https://$BASE_URL
+VITE_WS_BASE_URL=wss://$BASE_URL/join
 
 make -j
