abbfaca04fc57c5f8a7bb17829b1e0a686c33e51
Danny Piper <djpiper28@gmail.com>
2024-04-03 19:04:26 +0100

    feat: added storybook for header

---
+++ cahfrontend/src/components/typography/Header.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta } from "@storybook/react";
+import Header from "./Header";
+
+export default {
+  component: Header,
+} as Meta;
+
+export const Primary = {
+  args: {
+    text: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+  },
+};
