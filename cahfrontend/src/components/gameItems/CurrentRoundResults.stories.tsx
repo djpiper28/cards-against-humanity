04f015695c9c5f02c774739c424776ad8966d42a
Danny Piper <djpiper28@gmail.com>
2024-04-10 22:34:00 +0100

    feat: added current round result / status component

---
+++ cahfrontend/src/components/gameItems/CurrentRoundResults.stories.tsx
@@ -0,0 +1,150 @@
+import { Meta } from "@storybook/react";
+import CurrentRoundResults from "./CurrentRoundResults";
+
+export default {
+  component: CurrentRoundResults,
+} as Meta;
+
+export const InProgress = {
+  args: {
+    blackCard: {
+      name: "What is Batman's guilty pleasure?",
+      pack: "CAH",
+      cardsToPlay: 1,
+    },
+    showCards: false,
+    plays: [
+      {
+        winner: false,
+        whiteCards: [
+          {
+            id: "1",
+            name: "A windmill full of corpses",
+            pack: "CAH",
+          },
+        ],
+      },
+      {
+        winner: false,
+        whiteCards: [
+          {
+            id: "2",
+            name: "Dead babies",
+            pack: "CAH",
+          },
+        ],
+      },
+      {
+        winner: false,
+        whiteCards: [
+          {
+            id: "3",
+            name: "A super soaker full of cat pee",
+            pack: "CAH",
+          },
+        ],
+      },
+    ],
+  },
+};
+
+export const RoundEnd = {
+  args: {
+    blackCard: {
+      name: "What is Batman's guilty pleasure?",
+      pack: "CAH",
+      cardsToPlay: 1,
+    },
+    showCards: true,
+    plays: [
+      {
+        winner: false,
+        whiteCards: [
+          {
+            id: "1",
+            name: "A windmill full of corpses",
+            pack: "CAH",
+          },
+        ],
+      },
+      {
+        winner: true,
+        whiteCards: [
+          {
+            id: "2",
+            name: "Dead babies",
+            pack: "CAH",
+          },
+        ],
+      },
+      {
+        winner: false,
+        whiteCards: [
+          {
+            id: "3",
+            name: "A super soaker full of cat pee",
+            pack: "CAH",
+          },
+        ],
+      },
+    ],
+  },
+};
+
+export const RoundEndMultiCard = {
+  args: {
+    blackCard: {
+      name: "_ + _ = profit",
+      pack: "CAH",
+      cardsToPlay: 2,
+    },
+    showCards: true,
+    plays: [
+      {
+        winner: false,
+        whiteCards: [
+          {
+            id: "1",
+            name: "A windmill full of corpses",
+            pack: "CAH",
+          },
+          {
+            id: "11",
+            name: "7 dead and 22 injured",
+            pack: "CAH",
+          },
+        ],
+      },
+      {
+        winner: true,
+        whiteCards: [
+          {
+            id: "2",
+            name: "Dead babies",
+            pack: "CAH",
+          },
+          {
+            id: "22",
+            name: "A lifetime of sadness",
+            pack: "CAH",
+          },
+        ],
+      },
+      {
+        winner: false,
+        whiteCards: [
+          {
+            id: "3",
+            name: "A metric ton of bricks",
+            pack: "CAH",
+          },
+          {
+            id: "33",
+            name: "A white van man",
+            pack: "CAH",
+          },
+        ],
+      },
+    ],
+  },
+};
---
+++ cahfrontend/src/components/gameItems/CurrentRoundResults.tsx
@@ -0,0 +1,62 @@
+import Card from "./Card";
+
+interface WhiteCards {
+  id: string;
+  name: string;
+  pack: string;
+}
+
+interface PlayerPlay {
+  whiteCards: WhiteCards[];
+  winner: boolean;
+}
+
+interface BlackCard {
+  name: string;
+  pack: string;
+  cardsToPlay: number;
+}
+
+interface Props {
+  blackCard: BlackCard;
+  plays: PlayerPlay[];
+  showCards: boolean;
+}
+
+interface PlayerPlayProps {
+  play: PlayerPlay;
+  showCards: boolean;
+}
+
+function PlayerCard(props: Readonly<PlayerPlayProps>) {
+  return (
+    <div
+      class={`flex flex-row gap-2 border-2 border-white rounded-2xl ${props.play.winner ? "border-blue-500" : ""}`}
+    >
+      {props.play.whiteCards.map((card) => (
+        <Card
+          cardText={props.showCards ? card.name : "Cards Against Humanity"}
+          packName={props.showCards ? card.pack : ""}
+          isWhite={true}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default function CurrentRoundResults(props: Readonly<Props>) {
+  return (
+    <div class="flex flex-row gap-2">
+      <Card
+        cardText={props.blackCard.name}
+        packName={props.blackCard.pack}
+        isWhite={false}
+      />
+      <div class="flex flex-row flex-wrap gap-2">
+        {props.plays.map((play) => (
+          <PlayerCard play={play} showCards={props.showCards} />
+        ))}
+      </div>
+    </div>
+  );
+}
