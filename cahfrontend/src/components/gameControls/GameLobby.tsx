b15fbf91e26034dd0393d33df8bfc4454620add2
Danny Piper <djpiper28@gmail.com>
2024-04-15 17:21:42 +0100

    feat: cards and dity status of settings are now shown

--- cahfrontend/src/components/gameControls/GameLobby.tsx
+++ cahfrontend/src/components/gameControls/GameLobby.tsx
@@ -2,20 +2,32 @@ import { createSignal, onMount } from "solid-js";
 import LoadingSlug from "../loading/LoadingSlug";
 import { gameState } from "../../gameState/gameState";
 import { GameStateInfo } from "../../gameLogicTypes";
-import GameSettingsInput from "./GameSettingsInput";
+import GameSettingsInput, { Settings } from "./GameSettingsInput";
 import GameSettingsView from "./GameSettingsView";
 import RoundedWhite from "../containers/RoundedWhite";
 import Header from "../typography/Header";
 import PlayerList from "../gameItems/PlayerList";
 import { GamePlayerList } from "../../gameState/gamePlayersList";
+import CardsSelector from "./CardsSelector";
+import { apiClient } from "../../apiClient";
+import { GameLogicCardPack } from "../../api";
 
 interface Props {
   state: GameStateInfo;
   players: GamePlayerList;
   gameOwner: boolean;
+  cardPacks: GameLogicCardPack[];
 }
 
 function GameLobbyLoaded(props: Readonly<Props>) {
+  const [dirtyState, setDirtyState] = createSignal(false);
+  const [selectedPackIds, setSelectedPackIds] = createSignal(
+    props.state.settings.cardPacks.map((x) => x?.id ?? "no-id"),
+  );
+  const [gameSettings, setGameSettings] = createSignal<Settings>(
+    props.state.settings,
+  );
+
   return (
     <RoundedWhite>
       <Header
@@ -25,11 +37,27 @@ function GameLobbyLoaded(props: Readonly<Props>) {
         }'s Game`}
       />
       {props.gameOwner ? (
-        <GameSettingsInput
-          settings={props.state.settings}
-          setSettings={console.log}
-          errorMessage="TODO: Implement me!"
-        />
+        <>
+          <CardsSelector
+            cards={props.cardPacks}
+            selectedPackIds={selectedPackIds()}
+            setSelectedPackIds={(packs) => {
+              setDirtyState(true);
+              setSelectedPackIds(packs);
+            }}
+          />
+          <GameSettingsInput
+            settings={gameSettings()}
+            setSettings={(settings) => {
+              setDirtyState(true);
+              setGameSettings(settings);
+            }}
+            errorMessage="TODO: Implement me!"
+          />
+          <p id="settings-saved">
+            {dirtyState() ? "Settings are NOT saved..." : "Settings are saved."}
+          </p>
+        </>
       ) : (
         <GameSettingsView settings={props.state.settings} />
       )}
@@ -43,7 +71,29 @@ export default function GameLobby() {
   const [gameOwner, setGameOwner] = createSignal(false);
   const [players, setPlayers] = createSignal<GamePlayerList>([]);
 
-  onMount(() => {
+  const [packs, setPacks] = createSignal<GameLogicCardPack[]>([]);
+  const [errorMessage, setErrorMessage] = createSignal("");
+
+  onMount(async () => {
+    try {
+      const packs = await apiClient.res.packsList();
+      const cardPacksList: GameLogicCardPack[] = [];
+      const packData = packs.data;
+      for (let cardId in packData) {
+        cardPacksList.push({ ...packData[cardId] });
+      }
+      setPacks(
+        cardPacksList.sort((a, b) => {
+          if (!a.name || !b.name) return 0;
+          return a.name.localeCompare(b.name);
+        }),
+      );
+    } catch (err) {
+      console.error(err);
+      setErrorMessage(`Error getting card packs: ${err}`);
+    }
+
+    // Sync UI state with the source of truth
     gameState.onPlayerListChange = (players: GamePlayerList) => {
       setPlayers(players);
     };
@@ -57,6 +107,7 @@ export default function GameLobby() {
     };
     // Just incase the update has already happened
     gameState.emitState();
+
   });
 
   gameState.onPlayerListChange = (players: GamePlayerList) => {
@@ -78,12 +129,14 @@ export default function GameLobby() {
           state={state()}
           gameOwner={gameOwner()}
           players={players()}
+          cardPacks={packs()}
         />
       ) : (
         <div class="flex flex-grow justify-center items-center text-2xl">
           Waiting for lobby information <LoadingSlug />
         </div>
       )}
+      <p id="error-message" class="text-error-colour">{errorMessage()}</p>
     </>
   );
 }
