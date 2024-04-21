2cfd9161184ca731880cbcc3f171e8b51099c87e
Danny Piper <djpiper28@gmail.com>
2024-04-21 21:38:08 +0100

    fix: set path for jenkins

--- jenkinsBuild.sh
+++ jenkinsBuild.sh
@@ -2,6 +2,7 @@
 export GOPATH=/var/lib/jenkins/go
 export PATH=$PATH:/usr/local/bin
 export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:/usr/local/go/bin
 
 export BASE_URL=cards.djpiper28.co.uk/api
 export VITE_API_BASE_URL=https://$BASE_URL
