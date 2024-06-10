49c50a7931d517f367ddd8db1743df612d4ed934
Danny Piper <djpiper28@gmail.com>
2024-06-10 10:42:01 +0100

    feat: use the correct config during the e2e tests

--- e2e/e2e-start.sh
+++ e2e/e2e-start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd ../cahfrontend/ || exit 1
-export VITE_API_BASE_URL="http://localhost:8080"
+export VITE_API_BASE_URL="http://localhost:3255/api"
+export VITE_WS_BASE_URL="ws://localhost:3255/ws"
 pnpm dev
