5cb3015edee5cbe613457715e2636d0985188f96
Danny Piper <djpiper28@gmail.com>
2024-06-30 18:05:39 +0100

    feat: added test for starting game with min number of players

--- backend/game_in_game_test.go
+++ backend/game_in_game_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/djpiper28/cards-against-humanity/backend/gameLogic"
@@ -65,4 +66,72 @@ func (s *ServerTestSuite) TestStartGameTooFewPlayers() {
 	rpcMsg, err := network.DecodeAs[network.RpcCommandErrorMsg](msg)
 	assert.Nil(t, err, "Should be a command error message")
 	assert.NotEmpty(t, rpcMsg.Reason)
+}
+
+func (s *ServerTestSuite) TestStartGameEnoughPlayers() {
+	t := s.T()
+	t.Parallel()
+
+	client, err := NewTestGameConnection()
+	assert.NoError(t, err)
+	assert.NotNil(t, client)
+	defer client.Close()
+
+	// First message should be the player join broadcast, which we ignore
+	msgType, msg, err := client.Read()
+
+	assert.Nil(t, err, "Should be able to read (the initial game state)")
+	assert.True(t, len(msg) > 0, "Message should have a non-zero length")
+	assert.Equal(t, msgType, websocket.TextMessage)
+
+	onPlayerJoinMsg, err := network.DecodeAs[network.RpcOnPlayerJoinMsg](msg)
+	assert.Nil(t, err)
+	assert.Equal(t, client.PlayerId, onPlayerJoinMsg.Id, "The current user should have joined the game")
+
+	// Second message should be the state
+	msgType, msg, err = client.Read()
+
+	assert.Nil(t, err, "Should be able to read (the initial game state)")
+	assert.True(t, len(msg) > 0, "Message should have a non-zero length")
+	assert.Equal(t, msgType, websocket.TextMessage)
+
+	onJoinMsg, err := network.DecodeAs[network.RpcOnJoinMsg](msg)
+	assert.NoError(t, err, "Should be a join message")
+	assert.Equal(t, client.GameId, onJoinMsg.State.Id)
+	assert.Len(t, onJoinMsg.State.Players, 1)
+	assert.Contains(t, onJoinMsg.State.Players, gameLogic.Player{
+		Id:        client.PlayerId,
+		Name:      "Dave",
+		Points:    0,
+		Connected: true})
+
+	for i := 1; i < gameLogic.MinPlayers; i++ {
+		name := fmt.Sprintf("Player %d", i)
+		info, err := client.AddPlayer(name)
+		assert.NoError(t, err)
+
+		_, msg, err = client.Read()
+		assert.Nil(t, err, "Should be able to read the message")
+		assert.True(t, len(msg) > 0, "Message should have a non-zero length")
+
+		rpcMsg, err := network.DecodeAs[network.RpcOnPlayerJoinMsg](msg)
+		assert.NoError(t, err)
+		assert.Equal(t, rpcMsg.Id, info.PlayerId)
+		assert.Equal(t, rpcMsg.Name, name)
+	}
+
+	startGameMsg := network.RpcStartGameMsg{}
+	msgBytes, err := network.EncodeRpcMessage(startGameMsg)
+	assert.Nil(t, err)
+
+	err = client.Write(msgBytes)
+	assert.NoError(t, err)
+
+	_, msg, err = client.Read()
+	assert.Nil(t, err, "Should be able to read the message")
+	assert.True(t, len(msg) > 0, "Message should have a non-zero length")
+
+	rpcMsg, err := network.DecodeAs[network.RpcRoundInformationMsg](msg)
+	assert.NoError(t, err)
+	assert.Len(t, rpcMsg.YourHand, gameLogic.HandSize)
 }
