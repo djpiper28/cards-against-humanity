b422e8358b667b2ebed21bf7aade6e111baf4666
Danny Piper <djpiper28@gmail.com>
2024-06-30 19:15:43 +0100

    fix: better CSS for card selector

--- cahfrontend/src/components/gameItems/PlayerCards.tsx
+++ cahfrontend/src/components/gameItems/PlayerCards.tsx
@@ -15,7 +15,7 @@ interface Props {
 
 export default function PlayerCards(props: Readonly<Props>) {
   return (
-    <div class="flex flex-row flex-wrap gap-2 justify-between">
+    <div class="flex flex-row flex-wrap gap-2 justify-start">
       <For each={props.cards}>
         {(card) => {
           const isSelected = () =>
